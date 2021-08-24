3010b6811ddbd972209c1af257fc3c67d1795d0e
Benjy Barnett <benjybarnett98@hotmail.com>
2021-08-24 17:53:37 +0100

    first complete decode

--- fmri_scripts/SearchlightDecoding4Benjy.m
+++ fmri_scripts/SearchlightDecoding4Benjy.m
@@ -1,8 +1,9 @@
+tic
 cfg = [];
+cfg.nFold=5;
+cfg.gamma =0.2;
 statmask = 'stat_mask.nii'; % GM + has signal in each sub
-addpath('Utilities');
-addpath 'D:\bbarnett\Documents\ecobrain\spm12';
-addpath 'D:\bbarnett\Documents\ecobrain\fmri\fmri_analysis\'
+
 
 data_dir = 'D:\bbarnett\Documents\ecobrain\fmri\fmri_data\';
 subject='S02';
@@ -57,15 +58,26 @@ trials_needed = first_consc_idx:(first_uncon_idx-1); %if using all ratings we ca
 %full list of beta files, but only the trials_needed
 anim_idxs = {};
 inanim_idxs = {};
-
+Y = [];
 for trl = 1:length(trials_needed)
-    trial = reverse(char(SPM.xX.name(trl)));
-    disp(trial)
+    trial = trials_needed(trl);
+    trial = reverse(char(SPM.xX.name(trial)));
+  
     if trial(12) == '1' || trial(12) == '2'
         anim_idxs = [anim_idxs trl];
+        
     elseif trial(12) == '3' || trial(12) == '4'
         inanim_idxs = [inanim_idxs trl];   
-    end    
+        
+    end  
+   
+
+    %get labels (0 for low vis, 1 for high vis)
+    if str2double(trial(7)) < med %if lower than median split
+        Y = [Y 0]; %low vis label 
+    elseif str2double(trial(7)) >= med %if equal or greater than median split
+        Y = [Y 1]; %high vis label
+    end
 end
         
 
@@ -76,24 +88,35 @@ for trl = 1:length(trials_needed)
     beta_files = [beta_files file_name];
 end
 
+
 for file = 1:length(beta_files)
     file_name = beta_files{file};
-    disp(file_name)
+    %disp(file_name)
     [~, beta] = read_nii(strcat(beta_path,file_name));
     Betas(file,:) = beta(mask>0.5);
+    
 end
 
-anim_betas = Betas([anim_idxs{:}],:); %animate stim dataset
-inanim_betas = Betas([inanim_idxs{:}],:); %inanimate stim dataset
+X_anim = Betas([anim_idxs{:}],:); %animate stim dataset
+X_inanim = Betas([inanim_idxs{:}],:); %inanimate stim dataset
 
-X = betas;
+Y_anim = Y([anim_idxs{:}])'; %animate labels
+Y_inanim = Y([inanim_idxs{:}])'; %inanimate labels
 
-Y = labels; % get the training labels (visibility) 
 
 % downsample to get equal numbers per class
+%balance the number of trials of each condition in train and test samples
+idx_anim = balance_trials(double(Y_anim)+1,'downsample'); %idx is a Nclass x 1 cell, each cell with [Ntrials x 1] containing indices for trials of particular classs
+idx_inanim = balance_trials(double(Y_inanim)+1,'downsample');
 
+Y_anim = Y_anim(cell2mat(idx_anim));
+Y_inanim = Y_inanim(cell2mat(idx_inanim));
+
+X_anim = X_anim(cell2mat(idx_anim),:,:);
+X_inanim = X_inanim(cell2mat(idx_inanim),:,:);
 
 %% Loop over searchlights for decoding
+fprintf('Using %d trials per class \n',sum(Y_inanim==1))
 acc = nan(size(mask));
 for s = 1:length(mind)
         
@@ -102,20 +125,39 @@ for s = 1:length(mind)
         end
         
         % select those voxels
-        x = Betas{d}(:,mind{s});
-        x(:,isnan(x{d}(1,:))) = [];
+        X = X_inanim(:,mind{s}); %X is a nTrials vs. nVoxels in Searchlight matrix
+        X(:,isnan(X(1,:))) = []; %remove NaNs
+        
+
+        
+        num_trials = sum(Y_inanim==1);
+        
+        % n-fold cross-validation
+        folds = createFolds(cfg,Y_inanim); %returns cell array of length nFolds that contains in each cell indices of the trials belong to that particular fold
+        nTrials = size(X_inanim,1);
+        Xhat = zeros(nTrials,1);
         
         % do n Fold cross-validation 
-        for f = 1:nFold
-            
+        for f = 1:cfg.nFold
+            trainidx = setdiff(1:nTrials,folds{f}); %return indices of trials not in fold - used for training
+            testidx = folds{f}; %return indices of trials in fold - used for testing
+            x{1} = X(trainidx,:); x{2} = X(testidx,:); %split training and testing data into two cells in one cell array
+            y{1} = Y(trainidx); y{2} = Y(testidx); %split training and testing labels into two cells in one cell array
+
+            %fprintf('Decoding fold %d out of %d \n',f,cfg.nFold)
+                        
             % train
+            decoder = train_LDA(cfg,y{1}, x{1}');
             
             % test
+            Xhat(testidx) = decode_LDA(cfg, decoder, x{2}');
             
         end
         
+        yhat = Xhat > 0;
         % save accuracy per searchlight
-        acc(vind{s}) = mean(yhat == Y); 
+        acc(vind{s}) = mean(yhat == Y_inanim); 
         
 end
 
+toc
---
+++ fmri_scripts/main.m
@@ -0,0 +1,45 @@
+addpath('Utilities');
+addpath 'D:\bbarnett\Documents\ecobrain\spm12';
+addpath 'D:\bbarnett\Documents\ecobrain\fmri\fmri_analysis\'
+
+subjs = {'S01'
+        'S02'
+        'S03'
+        'S04'
+        'S05'
+        %'S06'
+        %'S07'
+        'S08'
+        'S09'
+        'S10'
+        'S11'
+        %'S12'
+        'S13'
+        'S14'
+        'S15'
+        'S16'
+        'S17'
+        %'S18'
+        'S19'
+        %'S20'
+        'S21'
+        'S22'
+        'S23'
+        'S24'
+        'S25'
+        'S26'
+        'S27'
+        %'S28'
+        'S29'
+        'S30'
+        'S31'
+        'S32'
+        'S35'
+        'S36'
+        'S37'};
+ 
+data_dir = 'D:\bbarnett\Documents\ecobrain\fmri\fmri_data\';
+
+%
+cfg = [];
+
